24104da52d98d372a49a7b543f2abc9cf6254d82
Jan Kampherbeek <tech@kampherbeek.com>
2020-12-07 19:40:57 +0100

    Enum for signs, rulership and exaltation

--- src/main/kotlin/com/radixpro/enigma/dedvm/core/References.kt
+++ src/main/kotlin/com/radixpro/enigma/dedvm/core/References.kt
@@ -33,6 +33,11 @@ enum class MundanePoints(override val id: Int, override val nameTxt: String): Po
     ASC(101,"Ascendant")
 }
 
+enum class EmptyPoints(override val id: Int, override val nameTxt: String): Points {
+    EXISTS_NOT(1000, "Does not exist"),
+    ERROR(1001, "Result of error")
+}
+
 
 /**
  * Supported aspects.
@@ -43,4 +48,19 @@ enum class Aspects(val degrees: Double, val orb: Double, val nameTxt: String) {
     TRIGON(120.0, 7.0, "Trigon"),
     SQUARE(90.0, 7.0, "Square"),
     SEXTILE(60.0, 6.0, "Sextile")
+}
+
+enum class Signs(val index: Int, val nameTxt:String, val abbr: String, val strong: Points, val weak: Points, val exalt: Points, val fall: Points) {
+    ARIES(1, "Aries", "AR", CelPoints.MARS, CelPoints.VENUS,  CelPoints.SUN, CelPoints.SATURN),
+    TAURUS(2,"Taurus", "TA", CelPoints.VENUS, CelPoints.MARS, CelPoints.MOON, EmptyPoints.EXISTS_NOT),
+    GEMINI(3, "Gemini", "GE", CelPoints.MERCURY, CelPoints.JUPITER, EmptyPoints.EXISTS_NOT, EmptyPoints.EXISTS_NOT),
+    CANCER(4, "Cancer", "CN", CelPoints.MOON, CelPoints.SATURN, CelPoints.JUPITER, CelPoints.MARS),
+    LEO(5, "Leo", "LE", CelPoints.SUN, CelPoints.URANUS, EmptyPoints.EXISTS_NOT, EmptyPoints.EXISTS_NOT),
+    VIRGO(6, "Virgo", "VI", CelPoints.MERCURY, CelPoints.NEPTUNE, CelPoints.MERCURY, CelPoints.VENUS),
+    LIBRA(7, "Libra", "LI", CelPoints.VENUS, CelPoints.MARS, CelPoints.SATURN, CelPoints.SUN),
+    SCORPIO(8, "Scorpio", "SC", CelPoints.PLUTO, CelPoints.VENUS, EmptyPoints.EXISTS_NOT, CelPoints.MOON),
+    SAGITTARIUS(9, "Sagittarius", "SA", CelPoints.JUPITER, CelPoints.MERCURY, EmptyPoints.EXISTS_NOT, EmptyPoints.EXISTS_NOT),
+    CAPRICORN(10, "Capricorn", "CP", CelPoints.SATURN, CelPoints.MOON, EmptyPoints.EXISTS_NOT, CelPoints.JUPITER),
+    AQUARIUS(11, "Aquarius", "AQ", CelPoints.URANUS, CelPoints.SUN, EmptyPoints.EXISTS_NOT, EmptyPoints.EXISTS_NOT),
+    PISCES(12, "Pisces", "PI", CelPoints.NEPTUNE, CelPoints.MERCURY, CelPoints.VENUS, CelPoints.MERCURY)
 }
