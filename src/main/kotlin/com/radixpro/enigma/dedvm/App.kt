4971b6c8b21994cd00c39c89bec64eee92cd65ac
Jan Kampherbeek <tech@kampherbeek.com>
2021-01-14 21:26:22 +0100

    Start of version 1.0. Updated version indications in Resource bundles and in pom.

---
+++ src/main/kotlin/com/radixpro/enigma/dedvm/App.kt
@@ -0,0 +1,16 @@
+/*
+ *
+ *  * Jan Kampherbeek, (c) 2020, 2021.
+ *  * EnigmaDedVM is open source.
+ *  * Please check the file copyright.txt in the root of the source for further details.
+ *
+ */
+
+package com.radixpro.enigma.dedvm
+
+object App {
+    @JvmStatic
+    fun main(args: Array<String?>) {
+        StartEnigma().run(args)
+    }
+}
